b1c94b0a5e4041e3980ff82002431abc7075f08c
Georgi Spasov <gvspasov@gmail.com>
2023-08-04 18:02:47 +0300

    feat: Add `:os_mon` for additional monitoring

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Roomy.MixProject do
   def application do
     [
       mod: {Roomy.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
