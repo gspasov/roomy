a634a79f52e90157f634cbae1da2aebeca74bd7b
Georgi Spasov <gvspasov@gmail.com>
2024-06-29 20:24:56 +0300

    feat: Add message encryption and participants section

---
+++ lib/roomy/crypto.ex
@@ -0,0 +1,29 @@
+defmodule Roomy.Crypto do
+  # 12 bytes (96 bits) is recommended for AES-GCM IV
+  @iv_length 12
+
+  def generate_key_pair() do
+    :crypto.generate_key(:ecdh, :secp256r1)
+  end
+
+  def generate_shared_secret(private_key, other_public_key) do
+    :crypto.compute_key(:ecdh, other_public_key, private_key, :secp256r1)
+  end
+
+  def derive_aes_key(shared_secret) do
+    :crypto.hash(:sha256, shared_secret)
+  end
+
+  def encrypt_message(aes_key, message) do
+    iv = :crypto.strong_rand_bytes(@iv_length)
+
+    {encrypted_message, tag} =
+      :crypto.crypto_one_time_aead(:aes_gcm, aes_key, iv, message, "", true)
+
+    {iv, encrypted_message, tag}
+  end
+
+  def decrypt_message(aes_key, {iv, encrypted_message, tag}) do
+    :crypto.crypto_one_time_aead(:aes_gcm, aes_key, iv, encrypted_message, "", tag, false)
+  end
+end
--- lib/roomy_web/live/room_live.ex
+++ lib/roomy_web/live/room_live.ex
@@ -1,4 +1,5 @@
 defmodule RoomyWeb.RoomLive do
+  alias Roomy.Crypto
   alias Roomy.Bus
   use RoomyWeb, :live_view
 
@@ -7,53 +8,68 @@ defmodule RoomyWeb.RoomLive do
     ~H"""
     <div class="flex flex-col h-full">
       <%= if is_nil(@name) do %>
-        <div>
-          <form class="flex justify-between gap-4" phx-submit="name:submit">
-            <input
-              type="text"
-              name="name"
-              class="w-full rounded-3xl border-slate-300 border px-5 focus:border-indigo-700"
-              placeholder="My name is.."
-              phx-debounce="100"
-              autocomplete="off"
-            />
-            <button class="bg-indigo-300 rounded p-3">
-              Join
-            </button>
+        <div class="py-10 px-24">
+          <h1 class="text-xl pb-8">Welcome to room <span class="italic"><%= @room_id %></span></h1>
+
+          <form class="flex-col justify-between gap-4" phx-submit="name:submit">
+            <label for="name">Choose your name</label>
+            <div class="flex relative">
+              <input
+                type="text"
+                name="name"
+                class="w-full h-12 rounded-3xl border-slate-300 border pr-12 px-5 focus:border-indigo-700"
+                placeholder="My name is..."
+                phx-debounce="100"
+                autocomplete="off"
+              />
+              <button class="absolute right-1 top-1 h-10 w-10 rounded-full bg-indigo-600 text-stone-100 hover:bg-indigo-500">
+                >
+              </button>
+            </div>
           </form>
         </div>
       <% else %>
-        <div id="chat_history" class="grow overflow-y-auto" phx-hook="ScrollToBottom">
-          <div
-            :for={{message, index} <- @chat_history |> Enum.reverse() |> Enum.with_index()}
-            class="flex mb-3"
-          >
-            <div key={"message-#{index}"} class="flex flex-col gap-1">
-              <div class="rounded-xl relative px-3 py-2">
-                <p class="max-w-prose break-words"><%= message %></p>
-              </div>
+        <div class="flex grow h-full">
+          <div class="bg-slate-300">
+            <h2 class="px-8 bg-slate-400">Participants</h2>
+            <div :for={name <- Map.keys(@participants)} class="px-4 py-2">
+              <%= name <> if(name == @name, do: " (You)", else: "") %>
             </div>
           </div>
+          <div class="flex flex-col grow">
+            <div id="chat_history" class="grow overflow-y-auto" phx-hook="ScrollToBottom">
+              <div
+                :for={{message, index} <- @chat_history |> Enum.reverse() |> Enum.with_index()}
+                class="flex mb-3"
+              >
+                <div key={"message-#{index}"} class="flex flex-col gap-1">
+                  <div class="rounded-xl relative px-3 py-2">
+                    <p class="max-w-prose break-words"><%= message %></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <form
+              class="relative flex justify-between pb-4 px-4 gap-4 mt-2"
+              phx-submit="message_box:submit"
+              phx-change="message_box:change"
+            >
+              <input
+                id="message_box"
+                type="text"
+                name="message"
+                class="w-full h-12 rounded-3xl border-slate-300 border pr-12 px-5 focus:border-indigo-700"
+                placeholder="Type your message..."
+                phx-debounce="50"
+                autocomplete="off"
+                value={@message_input}
+              />
+              <button class="absolute right-5 top-1 h-10 w-10 rounded-full bg-indigo-600 text-stone-100 hover:bg-indigo-500">
+                >
+              </button>
+            </form>
+          </div>
         </div>
-        <form
-          class="flex justify-between gap-4 mt-2"
-          phx-submit="message_box:submit"
-          phx-change="message_box:change"
-        >
-          <input
-            id="message_box"
-            type="text"
-            name="message"
-            class="w-full rounded-3xl border-slate-300 border px-5 focus:border-indigo-700"
-            placeholder="Type your message..."
-            phx-debounce="50"
-            autocomplete="off"
-            value={@message_input}
-          />
-          <button class="bg-indigo-300 rounded p-3">
-            Send
-          </button>
-        </form>
       <% end %>
     </div>
     """
@@ -67,6 +83,9 @@ defmodule RoomyWeb.RoomLive do
         name_input: "",
         message_input: "",
         chat_history: [],
+        participants: %{},
+        public_key: nil,
+        private_key: nil,
         room_id: room_id
       )
 
@@ -87,35 +106,88 @@ defmodule RoomyWeb.RoomLive do
   def handle_event(
         "message_box:submit",
         _params,
-        %{assigns: %{name: name, message_input: message, room_id: room_id}} = socket
+        %{
+          assigns: %{
+            name: name,
+            message_input: message,
+            room_id: room_id,
+            participants: participants
+          }
+        } = socket
       ) do
-    whole_message = "#{name}: #{message}"
-
-    room_id
-    |> room_topic()
-    |> Bus.publish(whole_message)
+    Enum.each(participants, fn {receiver_name, aes_key} ->
+      encrypted_message = Crypto.encrypt_message(aes_key, message)
+      topic = participant_topic(room_id, receiver_name)
+      Bus.publish(topic, {:message, name, encrypted_message})
+    end)
 
     {:noreply, assign(socket, message_input: "")}
   end
 
   @impl true
   def handle_event("name:submit", %{"name" => name}, %{assigns: %{room_id: room_id}} = socket) do
+    {public_key, private_key} = Crypto.generate_key_pair()
     room_id |> room_topic() |> Bus.subscribe()
+    room_id |> participant_topic(name) |> Bus.subscribe()
+    room_id |> room_topic() |> Bus.publish({:join, name, public_key})
 
-    {:noreply, assign(socket, name: name)}
+    {:noreply, assign(socket, name: name, public_key: public_key, private_key: private_key)}
   end
 
   @impl true
-  def handle_info({Bus, message}, %{assigns: %{chat_history: history}} = socket) do
+  def handle_info(
+        {Bus, {:message, name, message}},
+        %{assigns: %{chat_history: history, participants: participants}} = socket
+      ) do
+    decrypted_message =
+      participants
+      |> Map.fetch!(name)
+      |> Crypto.decrypt_message(message)
+
     new_socket =
       socket
-      |> assign(chat_history: [message | history])
+      |> assign(chat_history: ["#{name}: #{decrypted_message}" | history])
       |> push_event("message:new", %{is_sender: true})
 
     {:noreply, new_socket}
   end
 
+  @impl true
+  def handle_info(
+        {Bus, {action, name, public_key}},
+        %{
+          assigns: %{
+            room_id: room_id,
+            participants: participants,
+            name: my_name,
+            public_key: my_public_key,
+            private_key: private_key
+          }
+        } = socket
+      )
+      when action in [:join, :introduce] do
+    aes_key =
+      private_key
+      |> Crypto.generate_shared_secret(public_key)
+      |> Crypto.derive_aes_key()
+
+    if action == :join do
+      room_id |> participant_topic(name) |> Bus.publish({:introduce, my_name, my_public_key})
+    end
+
+    {:noreply, assign(socket, participants: Map.put(participants, name, aes_key))}
+  end
+
+  @impl true
+  def handle_info({Bus, {:leave, name}}, %{assigns: %{participants: participants}} = socket) do
+    {:noreply, assign(socket, participants: Map.delete(participants, name))}
+  end
+
   defp room_topic(room_id) do
     "/room/#{room_id}"
+  end
+
+  defp participant_topic(room_id, participant_name) do
+    "/room/#{room_id}/participant/#{participant_name}"
   end
 end
